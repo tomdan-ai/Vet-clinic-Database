149276316206081f240a422d2c74572490d1d1d6
Tom Daniel <88054834+tomdan-ai@users.noreply.github.com>
2023-06-13 10:46:06 +0100

    Create schema for animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  date_of_birth DATE,
+  escape_attempts INTEGER default 0,
+  neutered BOOLEAN,
+  weight_kg DECIMAL
 );
+
