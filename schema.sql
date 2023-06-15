9bc5005cf84e826c1aa87145ff8e97d1dfbb818c
Tom Daniel <tomudoh258@gmail.com>
2023-06-15 16:36:38 +0100

    Add new tables and modify animals table

--- schema.sql
+++ schema.sql
@@ -12,3 +12,27 @@ CREATE TABLE animals (
 -- Add the species column to the animals table
 ALTER TABLE animals ADD species VARCHAR(100);
 
+-- Create owners table in Vet_Clinic Database
+CREATE TABLE owners (
+  id PRIMARY KEY SERIAL,
+  full_name VARCHAR(100),
+  age INT
+);
+
+-- Create species Table in vet_clinic Datebase
+CREATE TABLE species (
+  id PRIMARY KEY SERIAL,
+  name VARCHAR(100)
+);
+
+-- Remove the column species
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- Add column species_id as a foreign key referencing the species table
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id);
+
+-- Add column owner_id as a foreign key referencing the owners table
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners(id);
